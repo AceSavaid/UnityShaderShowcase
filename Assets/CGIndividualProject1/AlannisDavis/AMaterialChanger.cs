9cc2e8e4f8fb290a3e2f3c2e836b23ba79c572ed
Ace Savaid <acetotheworld123@gmail.com>
2023-02-05 22:08:27 -0500

    final updates - Alannis

--- Assets/CGIndividualProject1/AlannisDavis/AMaterialChanger.cs
+++ Assets/CGIndividualProject1/AlannisDavis/AMaterialChanger.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AMaterialChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] List<Material> materials = new List<Material>();
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Material 1 Selected");
+            gameObject.GetComponent<MeshRenderer>().material = materials[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Material 2 Selected");
+            gameObject.GetComponent<MeshRenderer>().material = materials[1];
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Material 3 Selected");
+            gameObject.GetComponent<MeshRenderer>().material = materials[2];
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("Material 4 Selected");
+            gameObject.GetComponent<MeshRenderer>().material = materials[3];
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Debug.Log("Material 5 Selected");
+            gameObject.GetComponent<MeshRenderer>().material = materials[4];
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Debug.Log("Material 6 Selected");
+            gameObject.GetComponent<MeshRenderer>().material = materials[5];
+        }
+
     }
 }
